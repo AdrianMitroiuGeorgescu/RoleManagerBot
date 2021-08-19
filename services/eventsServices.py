79ad044db99c4a09c73ef85ddee2878d938b7b4f
Adrian Mitroiu <mitroiu.adrian@gmail.com>
2021-08-19 15:46:23 +0300

    Trying to fix barbut command when overwrites rolls with timeout
    Signed-off-by: Adrian Mitroiu <mitroiu.adrian@gmail.com>

--- services/eventsServices.py
+++ services/eventsServices.py
@@ -1,27 +1,34 @@
 import asyncio
 import random
 
+from discord import Embed, Message, Member
+
 from entities.members import MemberDto
+from main.bot import Bot
 
 
-async def check_barbut_command(bot, embed, message, payload):
+async def check_barbut_command(bot: Bot, embed: Embed, message: Message, payload):
     description        = embed.description.split(':')
     stake_is           = int(description[1])
     footer             = embed.footer.text.split(':')
     player_one         = int(footer[2])
     player_two         = int(footer[5])
-    allready_rolled    = []
     player_name        = None
     rolled_first_value = None
     to_roll_player_id  = player_one
 
-    if payload.emoji.name == '❌' and payload.member.id in [player_one, player_two]:
+    if payload.member.id not in [player_one, player_two] or len(embed.fields) > 1:
+        return
+
+    if payload.emoji.name == '❌':
+        embed.add_field(name=f'{payload.member.display_name}', value='Nu a acceptat invitația', inline=True)
+        embed.set_footer(text="")
+        await message.edit(embed=embed)
         await message.clear_reactions()
         return
 
     if embed.fields:
         for field in embed.fields:
-            allready_rolled.append(field.name)
             player_name        = field.name
             rolled_first_value = int(field.value)
 
@@ -31,45 +38,32 @@ async def check_barbut_command(bot, embed, message, payload):
     to_roll_player = bot.guild.get_member(int(to_roll_player_id))
 
     if payload.emoji.name == '✅' and payload.member.id == player_two:
-        await message.clear_reactions()
-        reactions = ['🎲']
-        for reaction in reactions:
-            await message.add_reaction(reaction)
-
-        def check(reaction, user):
-            return user in [payload.member, to_roll_player] and str(reaction.emoji) == '🎲'
-        try:
-            await bot.wait_for('reaction_add', timeout=30.0, check=check)
-        except asyncio.TimeoutError:
-            embed.add_field(name='Jocul a expirat', value='Nimeni nu a dat cu zarul', inline=True)
-            await message.edit(embed=embed)
-            await message.clear_reactions()
-
+        await prepare_game_after_accept(bot, embed, message, payload.member, to_roll_player)
         return
 
-    if payload.emoji.name == '🎲' \
-            and payload.member.id in [player_one, player_two] \
-            and payload.member.display_name not in allready_rolled:
-
+    if payload.emoji.name == '🎲' and payload.member.display_name != player_name:
         discord_member = bot.guild.get_member(payload.member.id)
         roll           = random.randrange(1, 100)
 
         embed.add_field(name=f'{discord_member.display_name}', value=roll, inline=True)
         await message.edit(embed=embed)
 
-        if 1 == len(allready_rolled):
+        print(embed.fields)
+        if 2 == len(embed.fields):
             if rolled_first_value > roll:
-                embed.add_field(name=f':crown: Câștigătorul este {player_name}', value=f'A câștigat {stake_is} XP',
+                embed.add_field(name=f':crown: Câștigătorul este {player_name}',
+                                value=f'A câștigat {stake_is} XP',
                                 inline=False)
                 await games_rewarding(bot, to_roll_player_id, payload.member.id, stake_is)
             elif rolled_first_value < roll:
                 embed.add_field(name=f':crown: Câștigătorul este {payload.member.display_name}',
-                                value=f'A câștigat {stake_is} XP', inline=False)
+                                value=f'A câștigat {stake_is} XP',
+                                inline=False)
                 await games_rewarding(bot, payload.member.id, to_roll_player_id, stake_is)
             elif rolled_first_value == roll:
                 embed.add_field(name=':crown: Egalitate', value='Nimeni nu a câștigat', inline=False)
 
-        elif 0 == len(allready_rolled):
+        elif 1 == len(embed.fields):
             def check(reaction, user):
                 return user == to_roll_player and str(reaction.emoji) == '🎲'
             try:
@@ -80,14 +74,30 @@ async def check_barbut_command(bot, embed, message, payload):
                                 value=f'A câștigat {stake_is} XP', inline=False)
 
                 await games_rewarding(bot, payload.member.id, to_roll_player.id, stake_is)
-
         embed.set_footer(text="")
         await message.edit(embed=embed)
         await message.clear_reactions()
         return
 
 
-async def games_rewarding(bot, winner: int, loser: int, stake: int):
+async def prepare_game_after_accept(bot: Bot, embed: Embed, message: Message, member: Member, to_roll_player: Member):
+    await message.clear_reactions()
+    reactions = ['🎲']
+    for reaction in reactions:
+        await message.add_reaction(reaction)
+
+    def check(reaction, user):
+        return user in [member, to_roll_player] and str(reaction.emoji) == '🎲'
+
+    try:
+        await bot.wait_for('reaction_add', timeout=30.0, check=check)
+    except asyncio.TimeoutError:
+        embed.add_field(name='Jocul a expirat', value='Nimeni nu a dat cu zarul', inline=True)
+        await message.edit(embed=embed)
+        await message.clear_reactions()
+
+
+async def games_rewarding(bot: Bot, winner: int, loser: int, stake: int):
     member_winner = MemberDto()
     member_loser  = MemberDto()
 
@@ -98,5 +108,4 @@ async def games_rewarding(bot, winner: int, loser: int, stake: int):
     member_loser.get_member(loser)
     member_loser.xp += -stake
     await member_loser.save(bot)
-
     return
